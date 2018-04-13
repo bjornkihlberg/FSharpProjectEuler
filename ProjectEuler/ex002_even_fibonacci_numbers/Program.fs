752257dddd972ae833049d193b02bbbcbeaeba51
hoppfull <hoppfull@gmail.com>
2018-04-13 18:32:10 +0800

    implement solution for project euler problem 2

--- ProjectEuler/ex002_even_fibonacci_numbers/Program.fs
+++ ProjectEuler/ex002_even_fibonacci_numbers/Program.fs
@@ -1,1 +1,13 @@
-﻿
+﻿let fibonacci : int seq =
+    let rec fib a b = seq { yield a; yield! fib b (a + b) }
+    fib 1 2
+
+let lessThan4Mil : int seq = Seq.takeWhile ((>) 4_000_000) fibonacci
+
+let isEven x = x % 2 = 0
+
+let evens : int seq = Seq.filter isEven lessThan4Mil
+
+let answer : int = Seq.sum evens
+
+printfn "answer = %i" answer
