5034620933b7d29fe3d41e31c716380d6a5bb708
hoppfull <hoppfull@gmail.com>
2018-04-13 17:41:31 +0800

    updated function name to accurate description

--- ProjectEuler/ex001_multiples_of_3_and_5/Program.fs
+++ ProjectEuler/ex001_multiples_of_3_and_5/Program.fs
@@ -1,8 +1,8 @@
 ﻿let ns : int seq = seq { let mutable i = 1 in while true do yield i; i <- i + 1 }
 
-let divisibleBy3And5 (x : int) : bool = x % 3 = 0 || x % 5 = 0
+let divisibleBy3Or5 (x : int) : bool = x % 3 = 0 || x % 5 = 0
 
-let answer : int = seq { for n in ns do if divisibleBy3And5 n then yield n }
+let answer : int = seq { for n in ns do if divisibleBy3Or5 n then yield n }
                    |> Seq.takeWhile ((>) 1000) |> Seq.sum
 
 printfn "answer = %i" answer
