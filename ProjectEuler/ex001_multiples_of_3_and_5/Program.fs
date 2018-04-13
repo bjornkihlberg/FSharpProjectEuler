dd59b00da5ea75063e15cf7e5aa3bd22441f37c9
hoppfull <hoppfull@gmail.com>
2018-04-13 17:26:44 +0800

    implemented solution for project euler problem ex001

--- ProjectEuler/ex001_multiples_of_3_and_5/Program.fs
+++ ProjectEuler/ex001_multiples_of_3_and_5/Program.fs
@@ -1,1 +1,8 @@
-﻿
+﻿let ns : int seq = seq { let mutable i = 1 in while true do yield i; i <- i + 1 }
+
+let divisibleBy3And5 (x : int) : bool = x % 3 = 0 || x % 5 = 0
+
+let answer : int = seq { for n in ns do if divisibleBy3And5 n then yield n }
+                   |> Seq.takeWhile ((>) 1000) |> Seq.sum
+
+printfn "answer = %i" answer
