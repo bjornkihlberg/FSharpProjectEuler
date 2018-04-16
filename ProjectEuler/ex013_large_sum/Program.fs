35fbe14b70d320fe0656a1adee3b09b50684b55b
hoppfull <hoppfull@gmail.com>
2018-04-16 12:27:30 +0800

    implemented solution for project euler problem 13

--- ProjectEuler/ex013_large_sum/Program.fs
+++ ProjectEuler/ex013_large_sum/Program.fs
@@ -1,1 +1,7 @@
-﻿
+﻿let data : bigint seq = System.IO.File.ReadLines "data_source.txt"
+                        |> Seq.map System.Numerics.BigInteger.Parse
+
+let answer : string = Seq.reduce (+) data
+                      |> string |> Seq.take 10 |> System.String.Concat
+
+printfn "answer = %s" answer
